0fba3e0357d1f2bea2ce6298e53d1e2ab5ed6cad
Tomas Santiago <thom.sgonzalez@gmail.com>
2015-03-26 11:28:13 -0600

    Ajustes para convertir binary to string

--- src/mx/com/mycloud/Demo.java
+++ src/mx/com/mycloud/Demo.java
@@ -7,12 +7,15 @@ public class Demo {
 		
 		TestWS ws = new TestWS();
 		String x = ws.Ws();
-		System.out.println("WS" + " " + x);
+		System.out.println("Main" + " " + x);
 		
 		try {
-			String s = args[0];
-			
-	        byte[] bytes = Convert.fromBinary(s);
+			String s = "I am programmer Java";
+			String binary = Convert.stringToBinary(s);
+	        System.out.println("Binary:" +binary);
+	    
+	        byte[] bytes = Convert.fromBinary(binary);
+	        System.out.println("Bytes:" +bytes);
 	  
 	        String string = new String(bytes, "UTF-8");
 	        System.out.println("String:" +string);
